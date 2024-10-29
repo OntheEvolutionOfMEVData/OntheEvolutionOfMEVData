b21348325f877da3dcf0b8b9673c481b15d04b91
unknown <chitianyang40@gmail.com>
2024-10-29 09:52:46 +0800

    update

--- scripts/Analysis_MEV_Activities_Financial_Metric.py
+++ scripts/Analysis_MEV_Activities_Financial_Metric.py
@@ -16,7 +16,7 @@ def get_list(infoDict, tag):
                 _list.append(int(infoDict[ii]["profit"]) / int(infoDict[ii]["revenue"]))
     return _list
 
-def generate_boxplot(ax, data, ylabel):
+def generate_boxplot(ax, data, ylabel,colors):
     bp = ax.boxplot(data, patch_artist=True, showfliers=False, vert=False)
     ax.set_yticklabels(['Mempool', 'Private', 'Mempool', 'Private'], rotation=90, va='center', ha='right')
     ax.set_xlabel(ylabel, fontsize=30)
@@ -35,53 +35,56 @@ def generate_boxplot(ax, data, ylabel):
     ax.text(xlim[1] + horizontal_offset, 3.5, 'Stage II', ha='left', va='center', fontsize=30, color='black',rotation=90)
     ax.text(xlim[1] + horizontal_offset, 1.5, 'Stage III', ha='left', va='center', fontsize=30, color='black',rotation=90)
 
-
-after_mempool = {}
-after_private = {}
-before_mempool = {}
-before_private = {}
-with open('../datasets/tx_profit.pkl','rb') as f:
-    tx_profit = pickle.load(f)
-with open('../datasets/tx_data.pkl','rb') as f:
-    tx_data = pickle.load(f)
-with open('../datasets/tx_state.pkl','rb') as f:
-    tx_state = pickle.load(f)
-for tx_hash in  tx_profit:
-    if tx_data[tx_hash]['BlockNumber'] >= 11834049 and tx_data[tx_hash]['BlockNumber'] <= 15537393:
-        if tx_hash in tx_state and tx_state[tx_hash] == "mempool":
-            before_mempool[tx_hash] = tx_profit[tx_hash]
-        if tx_hash in tx_state and tx_state[tx_hash] == "private":
-            before_private[tx_hash] = tx_profit[tx_hash]
-    if tx_data[tx_hash]['BlockNumber'] > 15537393:
-        if tx_hash in tx_state and tx_state[tx_hash] == "mempool":
-            after_mempool[tx_hash] = tx_profit[tx_hash]
-        if tx_hash in tx_state and tx_state[tx_hash] == "private":
-            after_private[tx_hash] = tx_profit[tx_hash]
-
-
-colors = ['green', '#1f77b4', 'green', '#1f77b4']   
-fig1, ax1 = plt.subplots(figsize=(5, 10))
-profit_data = [get_list(x, "profit") for x in [after_mempool, after_private, before_mempool, before_private]]
-generate_boxplot(ax1, profit_data, 'Profit(ETH)')
-plt.tight_layout()
-plt.savefig('Finance_A.pdf')
-plt.close()
+def main():
+    after_mempool = {}
+    after_private = {}
+    before_mempool = {}
+    before_private = {}
+    with open('../datasets/tx_profit.pkl','rb') as f:
+        tx_profit = pickle.load(f)
+    with open('../datasets/tx_data.pkl','rb') as f:
+        tx_data = pickle.load(f)
+    with open('../datasets/tx_state.pkl','rb') as f:
+        tx_state = pickle.load(f)
+    for tx_hash in  tx_profit:
+        if tx_data[tx_hash]['BlockNumber'] >= 11834049 and tx_data[tx_hash]['BlockNumber'] <= 15537393:
+            if tx_hash in tx_state and tx_state[tx_hash] == "mempool":
+                before_mempool[tx_hash] = tx_profit[tx_hash]
+            if tx_hash in tx_state and tx_state[tx_hash] == "private":
+                before_private[tx_hash] = tx_profit[tx_hash]
+        if tx_data[tx_hash]['BlockNumber'] > 15537393:
+            if tx_hash in tx_state and tx_state[tx_hash] == "mempool":
+                after_mempool[tx_hash] = tx_profit[tx_hash]
+            if tx_hash in tx_state and tx_state[tx_hash] == "private":
+                after_private[tx_hash] = tx_profit[tx_hash]
 
 
-fig2, ax2 = plt.subplots(figsize=(5, 10))
-profit_margin_data = [get_list(x, "profit margin") for x in [after_mempool, after_private, before_mempool, before_private]]
-generate_boxplot(ax2, profit_margin_data, 'Profit Margin(%)')
-plt.tight_layout()
-plt.savefig('Finance_B.pdf')
-plt.close()
+    colors = ['green', '#1f77b4', 'green', '#1f77b4']   
+    fig1, ax1 = plt.subplots(figsize=(5, 10))
+    profit_data = [get_list(x, "profit") for x in [after_mempool, after_private, before_mempool, before_private]]
+    generate_boxplot(ax1, profit_data, 'Profit(ETH)',colors)
+    plt.tight_layout()
+    plt.savefig('Finance_A.pdf')
+    plt.close()
+
+
+    fig2, ax2 = plt.subplots(figsize=(5, 10))
+    profit_margin_data = [get_list(x, "profit margin") for x in [after_mempool, after_private, before_mempool, before_private]]
+    generate_boxplot(ax2, profit_margin_data, 'Profit Margin(%)',colors)
+    plt.tight_layout()
+    plt.savefig('Finance_B.pdf')
+    plt.close()
 
 
 
-fig3, ax3 = plt.subplots(figsize=(5, 10))
-revenue_data = [get_list(x, "revenue") for x in [after_mempool, after_private, before_mempool, before_private]]
-generate_boxplot(ax3, revenue_data, 'Revenue(ETH)')
-plt.tight_layout()
-plt.savefig('Finance_C.pdf')
-plt.close()
+    fig3, ax3 = plt.subplots(figsize=(5, 10))
+    revenue_data = [get_list(x, "revenue") for x in [after_mempool, after_private, before_mempool, before_private]]
+    generate_boxplot(ax3, revenue_data, 'Revenue(ETH)',colors)
+    plt.tight_layout()
+    plt.savefig('Finance_C.pdf')
+    plt.close()
+
+if __name__ == "__main__":
+    main()
 
 
